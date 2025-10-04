ed733e692e23dc4fb6a3ebc854602bbb7202ab3d
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2025-10-04 23:56:03 +0500

    Task_4_completed

--- task_4.cpp
+++ task_4.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+int main()
+{
+    int num = 0, i = 0, sum = 0;
+    string output = "";
+    cout << "Enter an integer between 0 and 100 (inclusive) : ";
+    cin >> num;
+    if (num <= 0 || num > 100)
+    {
+        main();
+        return 0;
+    }
+    output = ((num % 2 == 0)
+                  ? "Even numbers between 0 and " + to_string(num) + " (inclusive) are : "
+                  : "Even numbers between 0 and " + to_string(num) + " are : ");
+    cout << endl
+         << output;
+    while (i <= num)
+    {
+        cout << i << " ";
+        sum += i;
+        i += 2;
+    }
+    cout << endl
+         << endl
+         << output
+         << sum;
+    return 0;
+}
 
 // n = no. of terms
 // a = first term
 // d = common difference
-int main()
+int main_1()
 {
     int num = 0, a = 2, d = 2, num_2 = 0;
     float n = 0;
@@ -14,7 +44,7 @@ int main()
 
     if (num <= 0 || num > 100)
     {
-        main();
+        main_1();
         return 0;
     }
     cout << endl;
