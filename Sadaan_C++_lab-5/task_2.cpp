c3cbcd806ef16ca487a9238ce3a65f442d0b5b84
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2025-10-11 09:53:25 +0500

    Task_2

--- Sadaan_C++_lab-5/task_2.cpp
+++ Sadaan_C++_lab-5/task_2.cpp
@@ -1,4 +1,4 @@
-// TASK_02 
+// TASK_02
 // Write a program that takes input an intger and then output both the individual digits of the input number and the sum of the digits.
 // For a negative number, use abs(number) => number will become positive.
 
@@ -8,6 +8,66 @@
 using namespace std;
 
 int main()
+{
+    long long num, power = 1, remainder, sum = 0;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    // Make positive
+    if (num < 0)
+    {
+        num = llabs(num); // or abs, but llabs is safer for long long
+    }
+
+    // save = num;
+
+    // Special case: if num is 0, we want to print digit “0”
+    if (num == 0)
+    {
+        cout << endl
+             << "The individual digits are: 0" << endl;
+        cout << "Sum of individual digits of " << num << " is: 0" << endl;
+        return 0;
+    }
+
+    // Compute highest power of 10 ≤ num
+    while (num / power >= 10)
+    {
+        power *= 10;
+    }
+
+    cout << endl
+         << "The individual digits are: ";
+
+    remainder = num;
+
+    while (power > 0)
+    {
+        long long digit = remainder / power;
+        cout << digit << " ";
+        sum += digit;
+
+        remainder = remainder % power;
+        power /= 10;
+    }
+
+    cout << endl
+         << "Sum of individual digits of " << num << " is: " << sum
+         << endl
+         << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+int main_my_written()
 {
     // Declaring Varibales
     int num = 0, sum = 0, remainder = 0, quotient = 1, save = 0, i;
